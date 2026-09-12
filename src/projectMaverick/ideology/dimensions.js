e9016f0a058ac5aae0a81f371ec38aebfc7b70eb
danielmartinez-maker <daniel.martinez@lomasaltas.edu.mx>
2026-09-11 18:06:18 -0600

    feat: add ideology dimensions

---
+++ src/projectMaverick/ideology/dimensions.js
@@ -0,0 +1,2 @@
+const DIMENSIONS = Object.freeze(['economics','fiscal','welfare','social','immigration','environment','guns','healthcare','education','institutional','partisan','intensity']);
+module.exports = { DIMENSIONS };
